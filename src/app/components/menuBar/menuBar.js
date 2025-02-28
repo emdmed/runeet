f9387ea15d4f9869fe2cd9a92e7665be2c7e2f37
emdmed <enrique.darderes@gmail.com>
2025-02-28 20:43:00 +0100

    changed activity monitor button icon

--- src/app/components/menuBar/menuBar.js
+++ src/app/components/menuBar/menuBar.js
@@ -15,7 +15,7 @@ import { Label } from "../../../components/ui/label"
 import { Switch } from "../../../components/ui/switch"
 
 //icons
-import { FolderPlus, RefreshCw, SquareActivity, Trash } from "lucide-react"
+import { FolderPlus, RefreshCw, Trash, Activity } from "lucide-react"
 
 const MenuBar = ({ menuBarActions, setMonitoringSettings, setIsCoolMode, isCoolMode, monitorTerminals, monitoringSettings }) => {
     return (
@@ -61,7 +61,7 @@ const MenuBar = ({ menuBarActions, setMonitoringSettings, setIsCoolMode, isCoolM
                 >
                     <RefreshCw />
                 </Button>
-                <div className="flex items-center">
+                <div className="flex items-center gap-2">
                     <Button
                         onClick={() =>
                             setMonitoringSettings((prev) => ({
@@ -75,7 +75,7 @@ const MenuBar = ({ menuBarActions, setMonitoringSettings, setIsCoolMode, isCoolM
                             : "text-stone-700"
                             } hover:bg-primary hover:text-black`}
                     >
-                        <SquareActivity />
+                        <Activity />
                     </Button>
                     <Button
                         className={`ps-0 ${monitoringSettings.autoMonitoring
