6d4791bbe7eaac86a0f5d9e13e797767f27b46c4
emdmed <enrique.darderes@gmail.com>
2025-03-02 23:12:24 +0100

    fix hydration error

--- src/app/components/settings/settings.js
+++ src/app/components/settings/settings.js
@@ -6,9 +6,20 @@ import {
     DialogTrigger,
     DialogClose
 } from "../../../components/ui/dialog"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from "../../../components/ui/alert-dialog"
 
 import { Button } from "../../../components/ui/button"
-import { Settings2 } from "lucide-react"
+import { Settings2, Bomb } from "lucide-react"
 import { Input } from "../../../components/ui/input"
 import { useEffect, useState } from "react"
 import { useSettings } from "../../hooks/useSettings"
@@ -24,19 +35,48 @@ const Settings = () => {
     }, [launchIDEcommandValue])
 
     return (<Dialog>
-        <DialogTrigger>
+        <DialogTrigger asChild>
             <Button className="p-2 bg-dark text-primary hover:bg-primary hover:text-black" size="icon"><Settings2 /></Button>
         </DialogTrigger>
         <DialogContent>
             <DialogHeader>
                 <DialogTitle>Settings</DialogTitle>
             </DialogHeader>
-            <div>
-                <span>Launch IDE command</span>
-                <Input value={launchIDEcommandValue} onChange={e => setLaunchIDECommandValue(e.target.value)} />
+            <div className="flex flex-col justify-start">
+                <span className="font-bold mb-1">Commands</span>
+                <div>
+                    <span>Launch IDE command</span>
+                    <Input value={launchIDEcommandValue} onChange={e => setLaunchIDECommandValue(e.target.value)} />
+                </div>
+
+                <span className="mb-1">Debug</span>
+                <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                        <div>
+                            <Button size="sm" variant="destructive">Clear stored data <Bomb/></Button>
+                        </div>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>
+                                You are about to delete all stored data, continue?
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={() => localStorage.clear()}>
+                                Continue
+                            </AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </div>
+
             <div className="flex justify-end gap-2">
-                <DialogClose>
+                <DialogClose asChild>
                     <Button variant="outline">Cancel</Button>
                 </DialogClose>
                 <Button onClick={() => saveSettings(newSettings)}>Save</Button>
